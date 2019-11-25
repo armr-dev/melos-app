033f65bff581f762a7702778c27ea6545883b9f8
Levi S. Lima <levidasilvalima.ldsl@gmail.com>
2019-11-24 21:20:21 -0400

    feat: validate cpf

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -3,11 +3,19 @@ const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
 
+const { cpf: cpfValidator } = require('cpf-cnpj-validator');
+
+
 router.post('/register', async (req, res) => {
     try {
         const { email } = req.body;
         if(await User.findOne({ email })) 
             return res.status(400).send({ error: 'User already exists'});
+        
+        const { cpf } = req.body;
+
+        if(cpfValidator.isValid(cpf) === false) 
+            return res.status(400).send({ error: 'User cpf is not valid' });
 
         const user = await User.create(req.body);
         
