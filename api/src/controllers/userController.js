663ba7a27f232a88b7d663764a376680a14ff19c
Levi S. Lima <levidasilvalima.ldsl@gmail.com>
2019-11-25 02:20:51 -0400

    feat: add token verifier to authentication

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -2,8 +2,19 @@ const express = require('express');
 
 const User = require('../models/user');
 const router = express.Router();
+const authConfing = require('../config/auth');
+
+
+const bcypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const { cpf: cpfValidator } = require('cpf-cnpj-validator');
+
+function generateToken(params) {
+    return jwt.sign({ params }, authConfing.secret, {
+        expiresIn: '1d',
+    });
+};
 
 
 router.post('/register', async (req, res) => {
@@ -20,12 +31,37 @@ router.post('/register', async (req, res) => {
         const user = await User.create(req.body);
         
         user.password = undefined;
-        return res.send({ user });
+        return res.send({ user, token: generateToken({ id: user.id }) });
     } catch (err) {
         console.log(err);
         res.status(400).send({error: 'Registration Failed'});
     }
-})
+});
+
+router.post('/authenticate', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email }).select('+password');
+        
+        if(!user) {
+            return res.status(400).send({ error: 'User not found!'});
+        }
+
+        if(!await bcypt.compare(password, user.password)) {
+            return res.status(400).send({ error: 'Invalid password' });
+        }
+
+        user.password = undefined;
+
+        return res.send({ user, token: generateToken({ id: user.id }) });
+
+    } catch(err) {
+
+        console.log(err);
+    }
+
+});
 
 module.exports = app => app.use('/auth', router);
 
