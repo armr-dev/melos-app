ddfdb50ee64e80b3f883c4ac93ff74184d15882d
armr-dev <armr.eng16@uea.edu.br>
2019-12-06 17:28:20 -0400

    Tela inicial com eventos

--- melos-react/src/assets/jss/material-dashboard-react/mapStyles.js
+++ melos-react/src/assets/jss/material-dashboard-react/mapStyles.js
@@ -1,27 +1,27 @@
+const colors = ['#00000']
+
 const mapStyle = {
     bottomMenu: {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        // justifyContent: 'space-around',
         position: 'absolute',
         height:'25vh',
         width:'100vw',
         backgroundColor:'#F2F2F2',
+        // backgroundColor: colors[0],
         bottom: 0,
         left: 0,
         borderRadius: '15px 15px 0px 0px',
         boxShadow:'0px 0px 6px 0px grey',
         transition: 'all ease-in-out 0.5s',
     },
-
+    
     bottomMenuOpened: {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         position: 'absolute',
-        // justifyContent: 'space-around',
-        // justifyContent: 'space-between',
         height:'92vh',
         width:'100vw',
         backgroundColor:'#F2F2F2',
@@ -32,8 +32,143 @@ const mapStyle = {
         transition: 'all ease-in-out 0.5s'
     },
 
+    eventList: {
+        overflow: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        width: '95vw',
+        height: 0,
+        transition: 'all ease-in-out 0.5s'
+    },
+
+    eventListOpened: {
+        overflow: 'auto',
+        padding: '0 1vw',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '95vw',
+        height: '100vh',
+        marginBottom: 10,
+        transition: 'all ease-in-out 0.5s',
+    },
+
+    eventCard: {
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: '20vh',
+        width: '100%',
+        margin: '1vw 0',
+        borderRadius: '10px',
+        backgroundColor: 'white',
+    },
+
+    cardContentStyle: {
+        flex:1,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        height:'100%',
+    },
+
+    imgStyle: {
+        flex:3,
+        height: '100%',
+        borderRadius: '10px 0px 0px 10px',
+    },
+
+    titleStyle: {
+        margin:'.5vh 0 0 2vw',
+        fontWeight: 'bold',
+        color: '#828282'
+    },
+
+    descriptionStyle: {
+        margin: '0 2vw .5vh 2vw',
+        textAlign: 'justify',
+        fontSize: '.9em',
+        overflow: 'hidden',
+    },
+
+    tagContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        marginLeft: '2vw',
+        marginBottom: '1%',
+
+    },
+
+    tagStyleShow: {
+        height: '1.1em',
+        borderRadius: '1.2em',
+        width: 'auto',
+        backgroundColor: '#F2C94C',
+        marginRight: '1vw',
+        padding: '0 5px 0 5px',
+        lineHeight: 1,       
+        verticalAlign: 'center',
+        textAlign:'center',
+        color:'white',
+        fontWeight: '500',
+    },
+    tagStyleAttraction: {
+        height: '1.1em',
+        borderRadius: '1.2em',
+        width: 'auto',
+        backgroundColor: '#9B51E0',
+        marginRight: '1vw',
+        padding: '0 5px 0 5px',
+        lineHeight: 1,       
+        verticalAlign: 'center',
+        textAlign:'center',
+        color:'white',
+        fontWeight: '500',
+    },
+    tagStyleEvent: {
+        height: '1.1em',
+        borderRadius: '1.2em',
+        width: 'auto',
+        backgroundColor: '#EB5757',
+        marginRight: '1vw',
+        padding: '0 5px 0 5px',
+        lineHeight: 1,       
+        verticalAlign: 'center',
+        textAlign:'center',
+        color:'white',
+        fontWeight: '500',
+    },
+    tagStyleTheater: {
+        height: '1.1em',
+        borderRadius: '1.2em',
+        width: 'auto',
+        backgroundColor: '#418BEE',
+        marginRight: '1vw',
+        padding: '0 5px 0 5px',
+        lineHeight: 1,       
+        verticalAlign: 'center',
+        textAlign:'center',
+        color:'white',
+        fontWeight: '500',
+    },
+    tagStyleMusic: {
+        height: '1.1em',
+        borderRadius: '1.2em',
+        width: 'auto',
+        backgroundColor: '#27AE60',
+        marginRight: '1vw',
+        padding: '0 5px 0 5px',
+        lineHeight: 1,       
+        verticalAlign: 'center',
+        textAlign:'center',
+        color:'white',
+        fontWeight: '500',
+    },
+
     dragButton: {
-        height: '8px',
+        height: '1vh',
         width: '7.5vw',
         backgroundColor: '#C4C4C4',
         marginTop: '1vh',
--- melos-react/src/views/Maps/Maps.js
+++ melos-react/src/views/Maps/Maps.js
@@ -1,30 +1,81 @@
 import React, { Component } from 'react';
 import mapStyle from 'assets/jss/material-dashboard-react/mapStyles.js'
 import Fab from '@material-ui/core/Fab'
-import Drawer from '@material-ui/core/Drawer';
+
 import Map from '../../components/Map'
+
+import teatro from '../../assets/img/teatro.jpg'
 import showIcon from '../../assets/img/showIcon.svg'
 import attractionIcon from '../../assets/img/attractionIcon.svg'
 import eventIcon from '../../assets/img/eventIcon.svg'
 import theaterIcon from '../../assets/img/theaterIcon.svg'
 import musicIcon from '../../assets/img/musicIcon.svg'
 import topMenuIcon from '../../assets/img/topMenuIcon.svg'
-// import { Container } from './styles';
+
+function selectColor(type) {
+  if(type == 'show')
+    return mapStyle.tagStyleShow
+  else if (type == 'attraction')
+    return mapStyle.tagStyleAttraction
+  else if (type == 'event')
+    return mapStyle.tagStyleEvent
+  else if (type == 'theater')
+    return mapStyle.tagStyleTheater
+  else
+    return mapStyle.tagStyleMusic
+}
+
+// function deleteList(list) {
+//   list = [
+//     {
+//       id: 'b',
+//       img: teatro,
+//       title: 'Evento',
+//       description: 'Descrição do evento: Lorem ipsum dolor sit amet.',
+//       type: 'attraction',
+//       tags: ['a', 'b', 'c'],
+//       },
+//   ]
+// }
 
 export default class Maps extends Component {
     state = {
-        opened: false,
-        searchTerm: ''
+        opened: true,
+        showEvents: true,
+        searchTerm: '',
+    }
+
+    deleteList = ( list ) => {
+      list = [
+        {
+          id: 'b',
+          img: teatro,
+          title: 'Evento',
+          description: 'Descrição do evento: Lorem ipsum dolor sit amet.',
+          type: 'attraction',
+          tags: ['a', 'b', 'c'],
+          },
+      ]
+      console.log(list);
     }
 
     toggleMenu = () => {
-    const { opened } = this.state;
-     this.setState({opened: !opened})
-     console.log(this.state);
+    const { opened, showEvents } = this.state;
+    this.setState({showEvents: !showEvents});
+    this.setState({opened: !opened})
+    console.log(this.state);
     }
 
     openMenu = () => {
-        this.setState({opened: true})
+      const { showEvents } = this.state;
+      this.setState({showEvents: !showEvents});
+      this.setState({opened: true})
+    }
+
+    showEvents = () => {
+      const { showEvents } = this.state;
+      this.setState({showEvents: !showEvents});
+      console.log(this.state);
     }
 
     handleSearch = e => {
@@ -45,7 +96,50 @@ export default class Maps extends Component {
 
      
   render() {
-      const {opened, searchTerm} = this.state;
+      const { opened, searchTerm, showEvents } = this.state;
+      const eventList = [
+        {
+        id: 'a',
+        img: teatro,
+        title: 'Evento',
+        description: 'Descrição do evento: Lorem ipsum dolor sit amet, consectetur adipiscing elit.  In sit amet suscipit dolor. Nunc sed facilisis sapien. Consectetur adipiscing elit. Sent sit amet ornare urna. Duis ne.',
+        type: 'show',
+        tags: ['#orquestra', '#piano'],
+        },
+        {
+        id: 'b',
+        img: teatro,
+        title: 'Evento',
+        description: 'Descrição do evento: Lorem ipsum dolor sit amet.',
+        type: 'attraction',
+        tags: ['a', 'b', 'c'],
+        },
+        {
+        id: 'c',
+        img: teatro,
+        title: 'Evento',
+        description: 'Descrição do evento: Lorem ipsum dolor sit amet, consectetur adipiscing elit.  In sit amet suscipit dolor. Nunc sed facilisis sapien. Consectetur adipiscing elit. Sent sit amet ornare urna. Duis ne.',
+        type: 'event',
+        tags: ['#orquestra', '#piano'],
+        },
+        {
+        id: 'd',
+        img: teatro,
+        title: 'Evento',
+        description: 'Descrição do evento: Lorem ipsum dolor sit amet, consectetur adipiscing elit.  In sit amet suscipit dolor. Nunc sed facilisis sapien. Consectetur adipiscing elit. Sent sit amet ornare urna. Duis ne.',
+        type: 'theater',
+        tags: ['#orquestra', '#piano'],
+        },
+        {
+        id: 'e',
+        img: teatro,
+        title: 'Evento',
+        description: 'Descrição do evento: Lorem ipsum dolor sit amet, consectetur adipiscing elit.  In sit amet suscipit dolor. Nunc sed facilisis sapien. Consectetur adipiscing elit. Sent sit amet ornare urna. Duis ne.',
+        type: 'music',
+        tags: ['#orquestra', '#piano'],
+        },
+      ];
+
     return (
             <div style={{position: 'relative', height: '100vh', width: '100vw'}}>
               <Map
@@ -54,7 +148,7 @@ export default class Maps extends Component {
                 containerElement={<div style={{ height: `100%`, width: '100vw'}} />}
                 mapElement={<div style={{ height: `100%` }}/>}
                 />
-              <div style={{position: 'absolute', top: '2.43vh', left: '4.87vw'}}>
+              <div style={{position: 'absolute', top: '2.43vh', left: '4.87vw'}} onClick={this.deleteList(eventList)}>
                 <img src={topMenuIcon}
                      style={{width: '6.81vw', height: '3.4vh'}}/>
               </div>
@@ -62,7 +156,10 @@ export default class Maps extends Component {
               <div 
                 style={opened ? mapStyle.bottomMenuOpened :  mapStyle.bottomMenu}
               >
-                <div style={mapStyle.dragButton} onClick={this.toggleMenu}/>
+                <div 
+                  style={mapStyle.dragButton} 
+                  onClick={this.toggleMenu}
+                />
                 <div style={{marginBottom:10}}>
                   <input 
                     type='text' 
@@ -72,8 +169,33 @@ export default class Maps extends Component {
                     style={mapStyle.searchInput}
                     onClick={this.openMenu}/>
                 </div>
-                <div style={{display: 'flex', flexDirection: 'row', position:'absolute', bottom:0}}>
-                  
+                <div 
+                  style={showEvents ? mapStyle.eventListOpened : mapStyle.eventList}
+                >  
+                  {eventList.map(item => (
+                    <div style={mapStyle.eventCard} key={item.id}>
+                      <div style={{overflow:'hidden', width:'29vw', height:'100%'}}>
+                        <img 
+                          src={item.img}
+                          style={mapStyle.imgStyle}
+                        />
+                      </div>
+                      <div style={mapStyle.cardContentStyle}>
+                        <div>
+                          <div style={mapStyle.titleStyle}>{item.title}</div>
+                          <div style={mapStyle.tagContainer}>
+                          {(item.tags).map(tag => (
+                            <div style={selectColor(item.type)}>{tag}</div>
+                            ))}
+                          </div> 
+                        </div>
+                        <div style={mapStyle.descriptionStyle}>{item.description}</div>
+                      </div>
+                    </div>
+                    ))}
+                </div>
+                
+                <div style={{display: 'flex', flexDirection: 'row'}}>
                   <div style={{display:'flex', flexDirection: 'column', alignItems: 'center'}}>
                     <Fab style={mapStyle.showButton}>
                       <img src={showIcon} style={{width:'43%'}}/>
