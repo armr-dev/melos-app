a3e8b9d53e6a5a7a37cc8e41139cf6c4434a103a
armr-dev <armr.eng16@uea.edu.br>
2019-12-06 21:38:36 -0400

    Pesquisa na tela inicial funcionando

--- melos-react/src/views/Maps/Maps.js
+++ melos-react/src/views/Maps/Maps.js
@@ -13,62 +13,116 @@ import musicIcon from '../../assets/img/musicIcon.svg'
 import topMenuIcon from '../../assets/img/topMenuIcon.svg'
 
 function selectColor(type) {
-  if(type == 'show')
-    return mapStyle.tagStyleShow
-  else if (type == 'attraction')
-    return mapStyle.tagStyleAttraction
-  else if (type == 'event')
-    return mapStyle.tagStyleEvent
-  else if (type == 'theater')
-    return mapStyle.tagStyleTheater
-  else
-    return mapStyle.tagStyleMusic
+  switch (type) {
+    case 'show':
+      return mapStyle.tagStyleShow;
+    case 'attraction':
+      return mapStyle.tagStyleAttraction;
+    case 'event':
+      return mapStyle.tagStyleEvent;
+    case 'theater':
+      return mapStyle.tagStyleTheater;
+
+    default:
+      return mapStyle.tagStyleMusic;
+  }
 }
 
-// function deleteList(list) {
-//   list = [
-//     {
-//       id: 'b',
-//       img: teatro,
-//       title: 'Evento',
-//       description: 'Descrição do evento: Lorem ipsum dolor sit amet.',
-//       type: 'attraction',
-//       tags: ['a', 'b', 'c'],
-//       },
-//   ]
-// }
+const initialList = [{
+    id: 'a',
+    img: teatro,
+    title: 'Evento',
+    description: 'Descrição do evento: Lorem ipsum dolor sit amet, consectetur adipiscing elit.  In sit amet suscipit dolor. Nunc sed facilisis sapien. Consectetur adipiscing elit. Sent sit amet ornare urna. Duis ne.',
+    type: 'show',
+    tags: ['#orquestra', '#piano'],
+  },
+  {
+    id: 'b',
+    img: teatro,
+    title: 'Evento',
+    description: 'Descrição do evento: Lorem ipsum dolor sit amet.',
+    type: 'attraction',
+    tags: ['#evento', '#atração', '#violão'],
+  },
+  {
+    id: 'c',
+    img: teatro,
+    title: 'Evento',
+    description: 'Descrição do evento: Lorem ipsum dolor sit amet, consectetur adipiscing elit.  In sit amet suscipit dolor. Nunc sed facilisis sapien. Consectetur adipiscing elit. Sent sit amet ornare urna. Duis ne.',
+    type: 'event',
+    tags: ['#orquestra', '#show'],
+  },
+  {
+    id: 'd',
+    img: teatro,
+    title: 'Evento',
+    description: 'Descrição do evento: Lorem ipsum dolor sit amet, consectetur adipiscing elit.  In sit amet suscipit dolor. Nunc sed facilisis sapien. Consectetur adipiscing elit. Sent sit amet ornare urna. Duis ne.',
+    type: 'theater',
+    tags: ['#atração', '#show'],
+  },
+  {
+    id: 'e',
+    img: teatro,
+    title: 'Evento',
+    description: 'Descrição do evento: Lorem ipsum dolor sit amet, consectetur adipiscing elit.  In sit amet suscipit dolor. Nunc sed facilisis sapien. Consectetur adipiscing elit. Sent sit amet ornare urna. Duis ne.',
+    type: 'music',
+    tags: ['#show', '#bacon'],
+  },
+  {
+    id: 'f',
+    img: teatro,
+    title: 'Evento',
+    description: 'Descrição do evento: Lorem ipsum dolor sit amet, consectetur adipiscing elit.  In sit amet suscipit dolor. Nunc sed facilisis sapien. Consectetur adipiscing elit. Sent sit amet ornare urna. Duis ne.',
+    type: 'music',
+    tags: ['#farofão', '#farinha'],
+  }]
 
 export default class Maps extends Component {
     state = {
         opened: true,
         showEvents: true,
+        emptyList: false,
         searchTerm: '',
+        eventList: [],
     }
 
-    deleteList = ( list ) => {
-      list = [
-        {
-          id: 'b',
-          img: teatro,
-          title: 'Evento',
-          description: 'Descrição do evento: Lorem ipsum dolor sit amet.',
-          type: 'attraction',
-          tags: ['a', 'b', 'c'],
-          },
-      ]
-      console.log(list);
+    deleteList = () => {
+      this.setState({ eventList:  []})
+    }
+
+    resetEvents = () => {
+      this.setState({ eventList: initialList, emptyList: false })
+    }
+
+    filterEvents = type => {
+      const { eventList, emptyList } = this.state;
+
+      var auxList = eventList
+      console.log(auxList);
+      
+      if (emptyList) {
+        auxList = initialList
+        this.setState({emptyList: false});
+      }
+
+      var filteredEvents = auxList.filter(function(item) {
+        if (item.type == type) {
+          return item;
+        }
+      })
+
+      this.setState({eventList: filteredEvents, emptyList: true})
     }
 
     toggleMenu = () => {
-    const { opened, showEvents } = this.state;
-    this.setState({showEvents: !showEvents});
-    this.setState({opened: !opened})
-    console.log(this.state);
+      const { opened, showEvents } = this.state;
+      this.setState({showEvents: !showEvents});
+      this.setState({opened: !opened})
+      console.log(this.state);
     }
 
     openMenu = () => {
-      const { showEvents } = this.state;
-      this.setState({showEvents: !showEvents});
+      this.setState({showEvents: true});
       this.setState({opened: true})
     }
 
@@ -79,66 +133,38 @@ export default class Maps extends Component {
     }
 
     handleSearch = e => {
-        const searchTerm = e.target.value;
-        this.setState({searchTerm})
-        if (this.timeout) clearTimeout(this.timeout);
-        this.timeout = setTimeout(() => {
+      const searchTerm = e.target.value;
+      this.setState({ searchTerm })
+      if (this.timeout) clearTimeout(this.timeout);
+      this.timeout = setTimeout(() => {
         if (searchTerm.length > 0) {
-            this.loadSearch();
+          this.loadSearch();
         }
-        }, 500);
+        else {
+          this.resetEvents();
+        }
+      }, 200);
     }
 
     loadSearch = async () => {
-        const { searchTerm } = this.state;
-        console.log(`Sua busca para o resultado ${searchTerm} retornou 10 termos`)
+      const { searchTerm, eventList } = this.state;
+
+      var results = eventList.filter((item) => {
+        if (item.tags.some(e => {return e.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1})) {
+          return item;
+        }
+      })
+
+      this.setState({eventList:results})
+      console.log(`Sua busca para o resultado ${searchTerm} retornou 10 termos`)
     }
 
-     
+  componentDidMount() {
+    this.setState({eventList: initialList})
+  };
+  
   render() {
-      const { opened, searchTerm, showEvents } = this.state;
-      const eventList = [
-        {
-        id: 'a',
-        img: teatro,
-        title: 'Evento',
-        description: 'Descrição do evento: Lorem ipsum dolor sit amet, consectetur adipiscing elit.  In sit amet suscipit dolor. Nunc sed facilisis sapien. Consectetur adipiscing elit. Sent sit amet ornare urna. Duis ne.',
-        type: 'show',
-        tags: ['#orquestra', '#piano'],
-        },
-        {
-        id: 'b',
-        img: teatro,
-        title: 'Evento',
-        description: 'Descrição do evento: Lorem ipsum dolor sit amet.',
-        type: 'attraction',
-        tags: ['a', 'b', 'c'],
-        },
-        {
-        id: 'c',
-        img: teatro,
-        title: 'Evento',
-        description: 'Descrição do evento: Lorem ipsum dolor sit amet, consectetur adipiscing elit.  In sit amet suscipit dolor. Nunc sed facilisis sapien. Consectetur adipiscing elit. Sent sit amet ornare urna. Duis ne.',
-        type: 'event',
-        tags: ['#orquestra', '#piano'],
-        },
-        {
-        id: 'd',
-        img: teatro,
-        title: 'Evento',
-        description: 'Descrição do evento: Lorem ipsum dolor sit amet, consectetur adipiscing elit.  In sit amet suscipit dolor. Nunc sed facilisis sapien. Consectetur adipiscing elit. Sent sit amet ornare urna. Duis ne.',
-        type: 'theater',
-        tags: ['#orquestra', '#piano'],
-        },
-        {
-        id: 'e',
-        img: teatro,
-        title: 'Evento',
-        description: 'Descrição do evento: Lorem ipsum dolor sit amet, consectetur adipiscing elit.  In sit amet suscipit dolor. Nunc sed facilisis sapien. Consectetur adipiscing elit. Sent sit amet ornare urna. Duis ne.',
-        type: 'music',
-        tags: ['#orquestra', '#piano'],
-        },
-      ];
+    const { opened, searchTerm, showEvents, eventList } = this.state;
 
     return (
             <div style={{position: 'relative', height: '100vh', width: '100vw'}}>
@@ -148,7 +174,7 @@ export default class Maps extends Component {
                 containerElement={<div style={{ height: `100%`, width: '100vw'}} />}
                 mapElement={<div style={{ height: `100%` }}/>}
                 />
-              <div style={{position: 'absolute', top: '2.43vh', left: '4.87vw'}} onClick={this.deleteList(eventList)}>
+              <div style={{position: 'absolute', top: '2.43vh', left: '4.87vw'}}>
                 <img src={topMenuIcon}
                      style={{width: '6.81vw', height: '3.4vh'}}/>
               </div>
@@ -197,35 +223,35 @@ export default class Maps extends Component {
                 
                 <div style={{display: 'flex', flexDirection: 'row'}}>
                   <div style={{display:'flex', flexDirection: 'column', alignItems: 'center'}}>
-                    <Fab style={mapStyle.showButton}>
+                    <Fab style={mapStyle.showButton} onClick={this.filterEvents.bind(this, 'show')}>
                       <img src={showIcon} style={{width:'43%'}}/>
                     </Fab>
                     <p style={{fontSize:'.95em', marginTop: 7}}>Show</p>
                   </div>
                   
                   <div style={{display:'flex', flexDirection: 'column', alignItems: 'center', marginLeft: '5.11vw'}}>
-                    <Fab style={mapStyle.attractionButton}>
+                    <Fab style={mapStyle.attractionButton} onClick={this.filterEvents.bind(this, 'attraction')}>
                       <img src={attractionIcon} style={{width:'43%'}}/>
                     </Fab>
                     <p style={{fontSize:'.95em', marginTop: 7}}>Atração</p>
                   </div>
                   
                   <div style={{display:'flex', flexDirection: 'column', alignItems: 'center', marginLeft: '5.11vw'}}>
-                    <Fab style={mapStyle.eventButton}>
+                    <Fab style={mapStyle.eventButton} onClick={this.filterEvents.bind(this, 'event')}>
                       <img src={eventIcon} style={{width:'43%'}}/>
                     </Fab>
                     <p style={{fontSize:'.95em', marginTop: 7}}>Evento</p>
                   </div>
                   
                   <div style={{display:'flex', flexDirection: 'column', alignItems: 'center', marginLeft: '5.11vw'}}>
-                    <Fab style={mapStyle.theaterButton}>
+                    <Fab style={mapStyle.theaterButton} onClick={this.filterEvents.bind(this, 'theater')}>
                       <img src={theaterIcon} style={{width:'43%'}}/>
                     </Fab>
                     <p style={{fontSize:'.95em', marginTop: 7}}>Teatro</p>
                   </div>
                   
                   <div style={{display:'flex', flexDirection: 'column', alignItems: 'center', marginLeft: '5.11vw'}}>
-                    <Fab style={mapStyle.musicButton}>
+                    <Fab style={mapStyle.musicButton} onClick={this.filterEvents.bind(this, 'music')}>
                       <img src={musicIcon} style={{width:'43%'}}/>
                     </Fab> 
                     <p style={{fontSize:'.95em', marginTop: 7}}>Música</p>
