991efd7206fe439eea22b90c1464b6c3314f4456
Levi S. Lima <levidasilvalima.ldsl@gmail.com>
2019-12-07 02:07:43 -0400

    chore: configure database connection

--- api/src/database/index.js
+++ api/src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://melos:EPJB1m3mBGbIe4CV@cluster0-crdcn.mongodb.net/test?retryWrites=true&w=majority', {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect('mongodb+srv://melos:EPJB1m3mBGbIe4CV@cluster0-crdcn.mongodb.net/test?retryWrites=true&w=majority', {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
 
 
 module.exports = mongoose;
