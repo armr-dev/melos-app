a5f04ef07c90fa254bcd8aa21b36f588a952990d
Stephanny <stephbarreto@me.com>
2019-12-08 20:27:28 -0400

    geolocation funcionando

--- melos-react/src/views/Maps/Geolocation.js
+++ melos-react/src/views/Maps/Geolocation.js
@@ -1,87 +1,164 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+// import {
+//   withScriptjs,
+//   withGoogleMap,
+//   GoogleMap,
+//   Marker
+// } from "react-google-maps";
+
 import GoogleMapReact from 'google-map-react';
-
-import { geolocated } from "react-geolocated";
-
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-// class MyMap extends Component {
-//   static defaultProps = {
-//     center: {
-//       lat: null,
-//       lng: null
-//     },
-    
-//     zoom: 11
-//   };
-
-//   render() {
-    
-//     return (
-//       // Important! Always set the container height explicitly
-//       <div style={{ height: '100vh', width: '100%' }}>
-//         <GoogleMapReact
-//           bootstrapURLKeys={{ key: 'AIzaSyCQgTwARDtFrtxT29EcwXt7_4PMQ7ypizM' }}
-//           defaultCenter={this.props.center}
-//           defaultZoom={this.props.zoom}
-//         >
-//           <AnyReactComponent
-//             lat={59.955413}
-//             lng={30.337844}
-//             text="My Marker"
-//           />
-//         </GoogleMapReact>
-//       </div>
-//     );
-//   }
-// }
-
-// export default MyMap;
-
-class Demo extends React.Component {
-  render() {
-      return !this.props.isGeolocationAvailable ? (
-          <div>Your browser does not support Geolocation</div>
-      ) : !this.props.isGeolocationEnabled ? (
-          <div>Geolocation is not enabled</div>
-      ) : this.props.coords ? (
-          <table> 
-              <tbody>
-                  <tr>
-                      <td>latitude</td>
-                      <td>{this.props.coords.latitude}</td>
-                  </tr>
-                  <tr>
-                      <td>longitude</td>
-                      <td>{this.props.coords.longitude}</td>
-                  </tr>
-                  <tr>
-                      <td>altitude</td>
-                      <td>{this.props.coords.altitude}</td>
-                  </tr>
-                  <tr>
-                      <td>heading</td>
-                      <td>{this.props.coords.heading}</td>
-                  </tr>
-                  <tr>
-                      <td>speed</td>
-                      <td>{this.props.coords.speed}</td>
-                  </tr>
-              </tbody>
-          </table>
-      ) : (
-          <div>Getting the location data&hellip; </div>
-      );
+class Geolocated extends Component {
   
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: 0,
+      lng: 0
+    };
   }
-}
 
-export default geolocated({
-  coordenadas:{
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        });
+      
+        console.log(this.state.lat);
+        console.log(this.state.lng);
+      },
+      err => {
+        console.log(err);
+        this.setState({ errorMessage: err.message });
+      }
+    );
     
-  },
-  positionOptions: {
-      enableHighAccuracy: false,
-  },
-  userDecisionTimeout: 5000,
-})(Demo);
+  }
+  
+  static defaultProps = {
+    center: {
+      lat: -3.0911771999999997,
+      lng: -60.069170299999996
+    },
+    zoom: 16
+  };
+  
+  render() {
+    
+    var x = this.state.lat;
+    var y = this.state.lng;
+    console.log(this.state);
+    console.log("latitude", x);
+    console.log("longitude", y);
+
+    return (
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyCQgTwARDtFrtxT29EcwXt7_4PMQ7ypizM'}}
+          defaultCenter={this.state}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={x}
+            lng={y}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+  
+}
+export default Geolocated;
+
+// const CustomSkinMap = withScriptjs(
+//   withGoogleMap(() => (
+//     <GoogleMap
+//       defaultZoom={13}
+//       defaultCenter={{ lat: x, lng: y }}
+//       defaultOptions={{
+//         scrollwheel: true,
+//         zoomControl: false,
+//         styles: [
+//           {
+//             featureType: "water",
+//             stylers: [
+//               { saturation: 43 },
+//               { lightness: -11 },
+//               { hue: "#0088ff" }
+//             ]
+//           },
+//           {
+//             featureType: "road",
+//             elementType: "geometry.fill",
+//             stylers: [
+//               { hue: "#ff0000" },
+//               { saturation: -100 },
+//               { lightness: 99 }
+//             ]
+//           },
+//           {
+//             featureType: "road",
+//             elementType: "geometry.stroke",
+//             stylers: [{ color: "#808080" }, { lightness: 54 }]
+//           },
+//           {
+//             featureType: "landscape.man_made",
+//             elementType: "geometry.fill",
+//             stylers: [{ color: "#ece2d9" }]
+//           },
+//           {
+//             featureType: "poi.park",
+//             elementType: "geometry.fill",
+//             stylers: [{ color: "#ccdca1" }]
+//           },
+//           {
+//             featureType: "road",
+//             elementType: "labels.text.fill",
+//             stylers: [{ color: "#767676" }]
+//           },
+//           {
+//             featureType: "road",
+//             elementType: "labels.text.stroke",
+//             stylers: [{ color: "#ffffff" }]
+//           },
+//           { featureType: "poi", stylers: [{ visibility: "off" }] },
+//           {
+//             featureType: "landscape.natural",
+//             elementType: "geometry.fill",
+//             stylers: [{ visibility: "on" }, { color: "#b8cb93" }]
+//           },
+//           { featureType: "poi.park", stylers: [{ visibility: "on" }] },
+//           {
+//             featureType: "poi.sports_complex",
+//             stylers: [{ visibility: "on" }]
+//           },
+//           { featureType: "poi.medical", stylers: [{ visibility: "on" }] },
+//           {
+//             featureType: "poi.business",
+//             stylers: [{ visibility: "simplified" }]
+//           }
+//         ]
+//       }}
+//     >
+//       <Marker position={{ lat: 40.748817, lng: -73.985428 }} />
+//     </GoogleMap>
+//   ))
+// );
+
+// export default function Maps() {
+//   return (
+//     <div>
+//     <CustomSkinMap
+//       googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCQgTwARDtFrtxT29EcwXt7_4PMQ7ypizM
+//       "
+//       loadingElement={<div style={{ height: `100%` }} />}
+//       containerElement={<div style={{ height: `100vh` }} />}
+//       mapElement={<div style={{ height: `100%` }} />}
+//       />
+//     </div>
+//   );
+// }
